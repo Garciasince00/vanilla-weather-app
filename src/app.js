c703fbd94017cb8d2ee834538731d085950b3550
Garciasince00 <estelagarcia1114@gmail.com>
2023-06-12 18:06:31 +0300

    added the search engine

--- src/app.js
+++ src/app.js
@@ -39,8 +39,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "3af343d165o3a456ae2ft30746bc3fea";
-let city = "Denver";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "3af343d165o3a456ae2ft30746bc3fea";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Denver");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
